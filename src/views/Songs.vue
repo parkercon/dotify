fd581cf5648af492167937c018c1dc05f27164eb
Parker Conrad <parkerconrad814@yahoo.com>
2022-02-10 12:36:25 -0800

    added edit/delete buttons

--- src/views/Songs.vue
+++ src/views/Songs.vue
@@ -1,16 +1,21 @@
 <template>
-<b-container class="bv-example-row" style="margin-top: 10%; background-color:floralwhite; outline:solid">
-  <b-row style="border-bottom: 1px solid black;">
-    <b-col style="font-weight:bold; border-right:1px solid black">Song Id</b-col>
-    <b-col style="font-weight:bold; border-right:1px solid black">Song Name</b-col>
-    <b-col style="font-weight:bold;">Song Date</b-col>
-  </b-row>
-  <b-row style="border-bottom: 1px solid black;" v-for="song in songs" :key="song.id">
-    <b-col style="border-right:1px solid black">{{song.songId}}</b-col>
-    <b-col style="border-right:1px solid black">{{song.songName}}</b-col>
-    <b-col>{{song.songDate}}</b-col>
-  </b-row>
-</b-container>
+<div>
+  <b-container class="bv-example-row" style="margin-top: 10%; background-color:floralwhite; outline:solid">
+    <b-row style="border-bottom: 1px solid black;">
+      <b-col style="font-weight:bold; border-right:1px solid black">Song Id</b-col>
+      <b-col style="font-weight:bold; border-right:1px solid black">Song Name</b-col>
+      <b-col style="font-weight:bold; border-right:1px solid black">Song Date</b-col>
+      <b-col style="font-weight:bold;">Edit Song</b-col>
+    </b-row>
+    <b-row style="border-bottom: 1px solid black;" v-for="song in songs" :key="song.id">
+      <b-col style="border-right:1px solid black">{{song.songId}}</b-col>
+      <b-col style="border-right:1px solid black">{{song.songName}}</b-col>
+      <b-col style="border-right:1px solid black">{{song.songDate}}</b-col>
+      <b-col><b-button variant="transparent"><b-icon icon="pencil"></b-icon></b-button>  <b-button class="delete" variant="transparent"><b-icon icon="trash"></b-icon></b-button></b-col>
+    </b-row>
+  </b-container>
+  <b-button style="margin-top: 10px;" variant="success">Add Song</b-button>
+</div>
 </template>
 
 <script>
@@ -25,6 +30,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.btn:focus {
+  box-shadow: none;
+}
+.btn:hover {
+  color: #17a2b8!important
+}
 
+.btn.delete:hover {
+  color: red!important
+}
 </style>
