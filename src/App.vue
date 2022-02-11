330c60fe407d27a6e4d7362c0919077b1259f593
Parker Conrad <parkerconrad814@yahoo.com>
2022-02-10 20:23:01 -0800

    router link

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-item href="/dotify/users">Users</b-nav-item>
+            <b-nav-item><router-link to="/dotify/users">Users</router-link></b-nav-item>
             <b-nav-item href="/dotify/playlists">Playlists</b-nav-item>
             <b-nav-item href="/dotify/songs">Songs</b-nav-item>
             <b-nav-item href="/dotify/artists">Artists</b-nav-item>
