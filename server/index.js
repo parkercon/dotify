24c3268d28824b3c52df4cceb9beb2810cd5c47e
Parker Conrad <parkerconrad814@yahoo.com>
2022-03-03 12:24:19 -0800

    deploy time pt2

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@
 import express from "express";
 // import cors
 import cors from "cors";
+
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+
 // import routes
 import Router from "./routes/api/routes.js";
 
