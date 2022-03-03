f12ef95f2e921922f56a7fda9c4f35bc282164c5
Parker Conrad <parkerconrad814@yahoo.com>
2022-03-03 12:21:33 -0800

    deploy time

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ import mysql from "mysql2";
 import dotenv from 'dotenv'
 dotenv.config()
 var db = mysql.createConnection({
-  host            : process.env.HOST,
+  host            : process.env.SQLHOST,
   user            : process.env.SQLUSER,
   password        : process.env.PASS,
   database        : process.env.DB
