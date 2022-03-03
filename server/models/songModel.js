987a6f5c449919a54cc1960363bcf3eb68e8573a
Parker Conrad <parkerconrad814@yahoo.com>
2022-03-03 13:31:35 -0800

    Update songModel.js

--- server/models/songModel.js
+++ server/models/songModel.js
@@ -8,7 +8,8 @@ function handleDisconnect() {
         console.log('error when connecting to db:', err);
         setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
       }                                     // to avoid a hot loop, and to allow our node script to
-    });        
+    });
+}        
 
 // Get All Songs
 export const getSongs = (result) => {
